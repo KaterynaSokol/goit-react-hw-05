fe71053bf59659d7e61f8313310ce8a2226dd2f2
KaterynaSokol <k.sokol2019@gmail.com>
2024-09-13 17:16:10 +0300

    final

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,5 +1,52 @@
+import { useEffect, useState } from "react";
+import { requestMoviesBySearchValue } from "../../services/api";
+import Loader from "../../components/Loader/Loader";
+import { useSearchParams } from "react-router-dom";
+import SearchForm from "../../components/SearchForm/SearchForm";
+import MovieList from "../../components/MovieList/MovieList";
+
 const MoviesPage = () => {
-  return <div></div>;
+  const [moviesSearch, setMoviesSearch] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const searchValue = searchParams.get("searchValue");
+
+  useEffect(() => {
+    if (!searchValue) return;
+
+    const fetchMoviesSearch = async () => {
+      try {
+        setIsLoading(true);
+        setError(null);
+        const { results } = await requestMoviesBySearchValue(searchValue);
+        setMoviesSearch(results);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchMoviesSearch();
+  }, [searchValue]);
+
+  const handleSubmit = (value) => {
+    setSearchParams({
+      searchValue: value,
+    });
+  };
+
+  return (
+    <div>
+      <SearchForm onSubmit={handleSubmit} />
+      {<MovieList moviesList={moviesSearch} />}
+
+      {isLoading && <Loader />}
+      {error && <p>Something went wrong...</p>}
+    </div>
+  );
 };
 
 export default MoviesPage;
--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+
 const NotFoundPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h3>Page Not Found</h3>
+      <Link to="/">Go back to Homepage</Link>
+    </div>
+  );
 };
 
 export default NotFoundPage;
